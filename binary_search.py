35ee25ab7d0a971eb29e3259a03487cc293d81f2
Max Pyhton Wizard <max.python.wizard@gmail.com>
2022-04-24 05:08:56 +0200

    binary_search() renamed to binary_search_rec(), also it returns position of target item instead of just True/False in binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,32 +1,34 @@
-list1 = [3,4,7,24,18,11,1,2]
+list1 = [3,4,7,24,18,11,1,2,5,8,111,-1]
 list1.sort()
 
-def binary_search(target, l):
-    print(l)
+def binary_search(target, l, ran = ()):
     len_l = len(l)
 
     if len_l == 1:
         if l[0] == target:
-            return True
+            return ran[0]
 
         else:
             return False
 
+    if ran == ():
+        ran = (0, len_l)
+
     middle = int(len_l/2)
-    print(l[middle])
+    # print(l[middle])
 
     if l[middle] == target:
-        return True
+        return ran[0] + middle
 
     elif l[middle] > target:
-        v = binary_search(target, l[:middle])
+        v = binary_search(target, l[:middle], (ran[0], ran[0] + middle))
 
     else:
-        v = binary_search(target, l[middle:])
+        v = binary_search(target, l[middle:], (ran[0] + middle, ran[1]))
         # print(val)
 
     return v
 
-ret = binary_search(11, list1)
+ret = binary_search(24, list1)
 
 print(ret)
