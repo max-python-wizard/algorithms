58635d38e75da729b15654bda5e2f2634739ef2b
Max Pyhton Wizard <max.python.wizard@gmail.com>
2022-04-24 05:54:42 +0200

    added binary search non recursive version as binary_search() in the binary_search.py file

--- binary_search.py
+++ binary_search.py
@@ -1,7 +1,10 @@
+from math import floor
+
 list1 = [3,4,7,24,18,11,1,2,5,8,111,-1]
 list1.sort()
 
-def binary_search(target, l, ran = ()):
+
+def binary_search_rec(target, l, ran = ()):
     len_l = len(l)
 
     if len_l == 1:
@@ -21,14 +24,44 @@ def binary_search(target, l, ran = ()):
         return ran[0] + middle
 
     elif l[middle] > target:
-        v = binary_search(target, l[:middle], (ran[0], ran[0] + middle))
+        v = binary_search_rec(target, l[:middle], (ran[0], ran[0] + middle))
 
     else:
-        v = binary_search(target, l[middle:], (ran[0] + middle, ran[1]))
+        v = binary_search_rec(target, l[middle:], (ran[0] + middle, ran[1]))
         # print(val)
 
     return v
 
-ret = binary_search(24, list1)
+def binary_search(target, l):
 
+    len_l = len(l)
+    left = 0
+    right = len_l
+
+
+    while(right - left > 2):
+        middle = left + floor((right - left) / 2)
+        l_middle = l[middle]
+
+        if l_middle == target:
+            return middle
+
+        elif target > l_middle:
+            left = middle
+
+        else:
+            right = middle
+
+    else:
+        if l[left] == target:
+            return left
+        elif l[left+1] == target:
+            return left+1
+        else:
+            return False
+
+ret = binary_search_rec(4, list1)
+print(ret)
+
+ret = binary_search(4, list1)
 print(ret)
